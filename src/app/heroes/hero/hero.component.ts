9ea7bc998051445b184214efbf2799aa2f79984b
Fabriany Pico <fabrianypico@gmail.com>
2024-01-04 21:29:11 -0500

    fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hereos-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+ public name: string = 'ironman'
+ public age:  number = 45
+
+ get capitalizeName():string {
+  return this.name.toUpperCase();
+ }
+
+ getHeroDescription():string {
+  return ` ${ this.name } - ${ this.age }`;
+ }
+
+ changeHero():void {
+   this.name = 'Spiderman'
+ }
+
+ changeAge(): void {
+   this.age = 25
+ }
+
+ ResectForm():void{
+  this.name = 'ironman';
+  this.age = 25;
+
+ }
+
+}
